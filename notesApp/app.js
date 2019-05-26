e915896cf34b716f9bce43d81abe53887d5572ae
talisonmaturana <talisondaniel@github.com>
2019-05-26 11:00:04 -0300

    Printing in color

--- notesApp/app.js
+++ notesApp/app.js
@@ -8,7 +8,9 @@
 //2. Create getNotes function that returns "Your notes..."
 //3. Export getNotes function
 //4. From app.js, load in and call the function printing message to console
-const validator = require('validator');
+//const validator = require('validator');
+// console.log(validator.isEmail('talison@hotmail.com'));
+//console.log(validator.isURL('https://talison.com'));
 
 const getNotes = require('./notes');
 
@@ -16,6 +18,17 @@ const msg = getNotes();
 
 console.log(msg);
 
-// console.log(validator.isEmail('talison@hotmail.com'));
-console.log(validator.isURL('https://talison.com'));
+//Challenge: Use the chalk library in your project
+
+//1. Install version 2.4.1 of chalk
+//2. Load chalk into app.js
+//3. Use it to print the string "Success!" to the console in green
+//4.Test your work
+
+//Bonus: Use docs to mess around with other styles. Make text bold and inversed.
+
+const chalk = require('chalk');
+console.log(chalk.green.bold.inverse('Success'));
+
+
 
